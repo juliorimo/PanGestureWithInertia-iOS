422a4aff62ebc14f82fcc69c721ea8ca5268d843
Julio Rivas <julio@lualabs.com>
2015-03-01 20:19:15 +0100

    Limits for pan gesture

--- TestPanGesture/ViewController.m
+++ TestPanGesture/ViewController.m
@@ -67,102 +67,6 @@
     }];
 }
 
-- (void)handlePanGesture:(UIPanGestureRecognizer *)panGestureRecognizer {
-
-    /*CGPoint translation = [recognizer translationInView:self.view];
-    recognizer.view.center = CGPointMake(recognizer.view.center.x + translation.x,
-                                         recognizer.view.center.y + translation.y);
-    [recognizer setTranslation:CGPointMake(0, 0) inView:self.view];*/
-
-    
-    switch (panGestureRecognizer.state) {
-    
-        case UIGestureRecognizerStateBegan:
-        {
-            NSLog(@"UIGestureRecognizerStateBegan");
-            
-//            [self pop_removeAnimationForKey:@"decelerate"];
-//            self.startBounds = self.bounds;
-        }
-        
-        case UIGestureRecognizerStateChanged:
-        {
-            NSLog(@"UIGestureRecognizerStateChanged");
-            
-            //Limits
-            CGPoint translation = [panGestureRecognizer translationInView:_scrollView];
-            NSLog(@"translation: %@",NSStringFromCGPoint(translation));
-            
-            //Limits
-            if(_scrollView.frame.origin.y+translation.y<_finalPoint.y )
-            {
-                _scrollView.frame = CGRectMake(_scrollView.frame.origin.x, _scrollView.frame.origin.y+translation.y, _scrollView.frame.size.width, _scrollView.frame.size.height-translation.y);
-                NSLog(@"scroll: %@",NSStringFromCGRect(_scrollView.frame));
-            }
-            
-            /*CGPoint translation = [panGestureRecognizer translationInView:self];
-            CGRect bounds = self.startBounds;
-            
-            CGFloat newBoundsOriginX = bounds.origin.x - translation.x;
-            CGFloat minBoundsOriginX = 0.0;
-            CGFloat maxBoundsOriginX = self.contentSize.width - bounds.size.width;
-            bounds.origin.x = fmax(minBoundsOriginX, fmin(newBoundsOriginX, maxBoundsOriginX));
-            
-            CGFloat newBoundsOriginY = bounds.origin.y - translation.y;
-            CGFloat minBoundsOriginY = 0.0;
-            CGFloat maxBoundsOriginY = self.contentSize.height - bounds.size.height;
-            bounds.origin.y = fmax(minBoundsOriginY, fmin(newBoundsOriginY, maxBoundsOriginY));
-            
-            self.bounds = bounds;*/
-        }
-    
-        break;
-        case UIGestureRecognizerStateEnded:
-        {
-            NSLog(@"UIGestureRecognizerStateEnded");
-            
-            /*CGPoint velocity = [panGestureRecognizer velocityInView:self];
-            if (self.bounds.size.width >= self.contentSize.width) {
-                velocity.x = 0;
-            }
-            if (self.bounds.size.height >= self.contentSize.height) {
-                velocity.y = 0;
-            }
-            velocity.x = -velocity.x;
-            velocity.y = -velocity.y;
-            NSLog(@"velocity: %@", NSStringFromCGPoint(velocity));
-            
-            POPDecayAnimation *decayAnimation = [POPDecayAnimation animation];
-            
-            
-            POPAnimatableProperty *prop = [POPAnimatableProperty propertyWithName:@"com.rounak.bounds.origin" initializer:^(POPMutableAnimatableProperty *prop) {
-                // read value
-                prop.readBlock = ^(id obj, CGFloat values[]) {
-                    NSLog(@"readBlock values: %f", values[0]);
-                    values[0] = [obj bounds].origin.x;
-                    values[1] = [obj bounds].origin.y;
-                };
-                // write value
-                prop.writeBlock = ^(id obj, const CGFloat values[]) {
-                    CGRect tempBounds = [obj bounds];
-                    NSLog(@"writeBlock values: %f", values[0]);
-                    tempBounds.origin.x = values[0];
-                    tempBounds.origin.y = values[1];
-                    [obj setBounds:tempBounds];
-                };
-                // dynamics threshold
-                prop.threshold = 0.01;
-            }];
-            decayAnimation.property = prop;
-            decayAnimation.velocity = [NSValue valueWithCGPoint:velocity];
-            [self pop_addAnimation:decayAnimation forKey:@"decelerate"];*/
-        }
-        break;
-        
-        default:
-        break;
-    }
-}
 
 
 -(void)animationDidFinish:(id)sender{
@@ -170,49 +74,74 @@
     [self pushBetty:nil];
 }
 
--(void)move:(id)sender {
+- (void)handlePanGesture:(UIPanGestureRecognizer *)panGestureRecognizer{
     
     //Pan gesture only over scroll
-    if([(UIPanGestureRecognizer*)sender view]!=_scrollView)return;
+    if([panGestureRecognizer view]!=_scrollView)return;
     
     //Start
-    if ([(UIPanGestureRecognizer*)sender state] == UIGestureRecognizerStateBegan) {
+    if ([panGestureRecognizer state] == UIGestureRecognizerStateBegan) {
         
         NSLog(@"--- Start ---");
         
-        _firstX = [[sender view] center].x;
-        _firstY = [[sender view] center].y;
+        _firstX = [[panGestureRecognizer view] center].x;
+        _firstY = [[panGestureRecognizer view] center].y;
     }
     
     //Movement point
-    CGPoint translatedPoint = [(UIPanGestureRecognizer*)sender translationInView:self.view];
+    CGPoint translatedPoint = [panGestureRecognizer translationInView:self.view];
 
+    //Vars
+    CGFloat x=0,y=0,width=0,height=0;
+    
     //Orientation
     if(_bettyOrientation==WRTemplateBettyOrientationVertical){
 
         //Move Y
         translatedPoint = CGPointMake(_firstX, +translatedPoint.y+_firstY);
         
+        //Final positions
+        x = translatedPoint.x - [panGestureRecognizer view].bounds.size.width / 2;
+        y = translatedPoint.y - [panGestureRecognizer view].bounds.size.height / 2;
+        
+        //Limit top
+        if(y<_finalPoint.y)y=_finalPoint.y;
+        
+        //Limit down
+        if(y>_initialPoint.y)y=_initialPoint.y;
+        
+        //Size
+        width = [panGestureRecognizer view].bounds.size.width;
+        height = _initialPoint.y-y+_bettyButton.frame.size.height;
+        
     }else if(_bettyOrientation==WRTemplateBettyOrientationHorizonal){
         
         //Move X
         translatedPoint = CGPointMake(_firstX+translatedPoint.x, _firstY);
+        
+        //Final positions
+        x = translatedPoint.x - [panGestureRecognizer view].bounds.size.width / 2;
+        y = translatedPoint.y - [panGestureRecognizer view].bounds.size.height / 2;
+        
+        //Limit left
+        //TODO
+        
+        //Limit right
+        //TODO
+        
+        //Size
+        width = _initialPoint.x-x+_bettyButton.frame.size.width;
+        height = [panGestureRecognizer view].bounds.size.height;
     }
     
-    //Movement
-    CGFloat x = translatedPoint.x - [sender view].bounds.size.width / 2;
-    CGFloat y = translatedPoint.y - [sender view].bounds.size.height / 2;
-    
-    CGFloat width = [sender view].bounds.size.width;
-    CGFloat height = _initialPoint.y-y+_bettyButton.frame.size.height;//  [sender view].bounds.size.height;
-    
+    //New frame
     NSLog(@"translatedPoint: %@",NSStringFromCGPoint(CGPointMake(x, y)));
-    [[sender view] setFrame:CGRectMake(x, y, width, height)];
-    
-    //[[sender view] setCenter:translatedPoint];
+    [[panGestureRecognizer view] setFrame:CGRectMake(x, y, width, height)];
+    _bettyButton.frame = CGRectMake(x, y, _bettyButton.frame.size.width, _bettyButton.frame.size.height);
+    //[[panGestureRecognizer view] setCenter:translatedPoint];
     
     //End animation
-    if ([(UIPanGestureRecognizer*)sender state] == UIGestureRecognizerStateEnded) {
+    if ([panGestureRecognizer state] == UIGestureRecognizerStateEnded) {
         
         NSLog(@"--- End ---");
         
@@ -224,7 +153,7 @@
         if(_bettyOrientation==WRTemplateBettyOrientationVertical){
         
             //Velocity
-            velocityY = (0.2*[(UIPanGestureRecognizer*)sender velocityInView:self.view].y);
+            velocityY = (0.2*[panGestureRecognizer velocityInView:self.view].y);
             
             //Final position
             finalX = _firstX;
@@ -233,7 +162,7 @@
         }else if(_bettyOrientation==WRTemplateBettyOrientationHorizonal){
         
             //Velocity
-            velocityX = (0.2*[(UIPanGestureRecognizer*)sender velocityInView:self.view].x);
+            velocityX = (0.2*[panGestureRecognizer velocityInView:self.view].x);
             
             //Final point
             finalX = translatedPoint.x + velocityX;
@@ -276,7 +205,7 @@
         [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
         [UIView setAnimationDelegate:self];
         //[UIView setAnimationDidStopSelector:@selector(animationDidFinish:)];
-        //[[sender view] setCenter:CGPointMake(finalX, finalY)];
+        //[[panGestureRecognizer view] setCenter:CGPointMake(finalX, finalY)];
         [UIView commitAnimations];
     }
 }
@@ -311,11 +240,109 @@
     [self.view addSubview:_bettyButton];
 
     //Add pan gesture
-    UIPanGestureRecognizer *gestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(move:)];
+    UIPanGestureRecognizer *gestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)];
     [gestureRecognizer setMinimumNumberOfTouches:1];
     [gestureRecognizer setMaximumNumberOfTouches:1];
     [_scrollView addGestureRecognizer:gestureRecognizer];
 }
+
+//- (void)handlePanGesture:(UIPanGestureRecognizer *)panGestureRecognizer {
+//
+//    /*CGPoint translation = [recognizer translationInView:self.view];
+//    recognizer.view.center = CGPointMake(recognizer.view.center.x + translation.x,
+//                                         recognizer.view.center.y + translation.y);
+//    [recognizer setTranslation:CGPointMake(0, 0) inView:self.view];*/
+//
+//
+//    switch (panGestureRecognizer.state) {
+//
+//        case UIGestureRecognizerStateBegan:
+//        {
+//            NSLog(@"UIGestureRecognizerStateBegan");
+//
+////            [self pop_removeAnimationForKey:@"decelerate"];
+////            self.startBounds = self.bounds;
+//        }
+//
+//        case UIGestureRecognizerStateChanged:
+//        {
+//            NSLog(@"UIGestureRecognizerStateChanged");
+//
+//            //Limits
+//            CGPoint translation = [panGestureRecognizer translationInView:_scrollView];
+//            NSLog(@"translation: %@",NSStringFromCGPoint(translation));
+//
+//            //Limits
+//            if(_scrollView.frame.origin.y+translation.y<_finalPoint.y )
+//            {
+//                _scrollView.frame = CGRectMake(_scrollView.frame.origin.x, _scrollView.frame.origin.y+translation.y, _scrollView.frame.size.width, _scrollView.frame.size.height-translation.y);
+//                NSLog(@"scroll: %@",NSStringFromCGRect(_scrollView.frame));
+//            }
+//
+//            /*CGPoint translation = [panGestureRecognizer translationInView:self];
+//            CGRect bounds = self.startBounds;
+//
+//            CGFloat newBoundsOriginX = bounds.origin.x - translation.x;
+//            CGFloat minBoundsOriginX = 0.0;
+//            CGFloat maxBoundsOriginX = self.contentSize.width - bounds.size.width;
+//            bounds.origin.x = fmax(minBoundsOriginX, fmin(newBoundsOriginX, maxBoundsOriginX));
+//
+//            CGFloat newBoundsOriginY = bounds.origin.y - translation.y;
+//            CGFloat minBoundsOriginY = 0.0;
+//            CGFloat maxBoundsOriginY = self.contentSize.height - bounds.size.height;
+//            bounds.origin.y = fmax(minBoundsOriginY, fmin(newBoundsOriginY, maxBoundsOriginY));
+//
+//            self.bounds = bounds;*/
+//        }
+//
+//        break;
+//        case UIGestureRecognizerStateEnded:
+//        {
+//            NSLog(@"UIGestureRecognizerStateEnded");
+//
+//            /*CGPoint velocity = [panGestureRecognizer velocityInView:self];
+//            if (self.bounds.size.width >= self.contentSize.width) {
+//                velocity.x = 0;
+//            }
+//            if (self.bounds.size.height >= self.contentSize.height) {
+//                velocity.y = 0;
+//            }
+//            velocity.x = -velocity.x;
+//            velocity.y = -velocity.y;
+//            NSLog(@"velocity: %@", NSStringFromCGPoint(velocity));
+//
+//            POPDecayAnimation *decayAnimation = [POPDecayAnimation animation];
+//
+//
+//            POPAnimatableProperty *prop = [POPAnimatableProperty propertyWithName:@"com.rounak.bounds.origin" initializer:^(POPMutableAnimatableProperty *prop) {
+//                // read value
+//                prop.readBlock = ^(id obj, CGFloat values[]) {
+//                    NSLog(@"readBlock values: %f", values[0]);
+//                    values[0] = [obj bounds].origin.x;
+//                    values[1] = [obj bounds].origin.y;
+//                };
+//                // write value
+//                prop.writeBlock = ^(id obj, const CGFloat values[]) {
+//                    CGRect tempBounds = [obj bounds];
+//                    NSLog(@"writeBlock values: %f", values[0]);
+//                    tempBounds.origin.x = values[0];
+//                    tempBounds.origin.y = values[1];
+//                    [obj setBounds:tempBounds];
+//                };
+//                // dynamics threshold
+//                prop.threshold = 0.01;
+//            }];
+//            decayAnimation.property = prop;
+//            decayAnimation.velocity = [NSValue valueWithCGPoint:velocity];
+//            [self pop_addAnimation:decayAnimation forKey:@"decelerate"];*/
+//        }
+//        break;
+//
+//        default:
+//        break;
+//    }
+//}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
